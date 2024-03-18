648ab59684cce9ae9ce7c1ab8f2d6c146b2b6bae
Ali Akram <112803350+Akram-00@users.noreply.github.com>
2024-03-18 00:03:34 +0000

    Basic setup and Navbar created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './App.css'
+import { Button } from "@material-tailwind/react";
+import { NavbarWithMegaMenu } from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+     <NavbarWithMegaMenu/>
+    </>
+  )
+}
+
+export default App
